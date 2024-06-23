ab25a8546bc25b4bbe7af59065423d05fcf9530c
philipmogul <philipp1mogul@gmail.com>
2024-06-24 01:54:43 +0300

    Ruby Coder Index

---
+++ index.rb
@@ -0,0 +1,19 @@
+
+myname = "Philip Mumo"
+myoccupation = "Software Engineer"
+myage = '30'
+activity = "I\'m currently learning Ruby "
+
+puts "My name => " + myname + " , My occupation => " + myoccupation + " , My age = " + myage 
+puts " "
+print activity 
+puts " "
+
+
+# Arrays 
+arr = ["phone","tablet","laptop","ftmo"]
+puts "Array pointing at =>" + arr[0]
+arr << "1M in Funding"
+puts "New element at end of array =>" + arr[4]
+puts " "
+puts arr.inspect
